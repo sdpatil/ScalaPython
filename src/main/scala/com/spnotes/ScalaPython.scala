2a7ad35cb1e98152a26f80eefd8cb372d94d57e6
Sunil Patil <pppsunil@gmail.com>
2016-02-12 16:44:34 -0800

    Make fileName as parameter

--- src/main/scala/com/spnotes/ScalaPython.scala
+++ src/main/scala/com/spnotes/ScalaPython.scala
@@ -9,11 +9,19 @@ object ScalaPython {
 
   def main(argv:Array[String]): Unit ={
     System.out.println("Entering ScalaPython.main")
+    if(argv.length != 1){
+      println("Please provide 1 parameters <inputFileName>")
+      System.exit(1)
+    }
+    val fileName = argv(0)
     val sparkConf = new SparkConf().setAppName("ScalaPython").setMaster("local")
     val sparkContext = new SparkContext(sparkConf)
 
-    val lines = sparkContext.textFile("/Users/sunilpatil/Downloads/pumpsmall.log")
+    val lines = sparkContext.textFile(fileName)
+    println("Print lines from original file")
+    lines.foreach(println)
     val lines1 = lines.pipe("src/main/resources/echo.py")
+    println("Print lines returned from python")
 
     lines1.foreach(println)
     System.out.println("Exiting ScalaPython.main")
